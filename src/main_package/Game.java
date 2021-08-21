70512abad143e4ebfd1e3410f0b26ffa11d20fa3
Kaku <79337205+Kakulive@users.noreply.github.com>
2021-08-21 15:09:50 +0200

    added the move validation so pawns can't move more than 1 place at a time

--- src/main_package/Game.java
+++ src/main_package/Game.java
@@ -25,21 +25,29 @@ public class Game {
         View.printBoard(board);
         int[] pawnCoordinates = userHandler.choosePawn(board);
         Pawn pawnToMove = board[pawnCoordinates[0]][pawnCoordinates[1]];
+
         while (!validator.verifyCorrectPlayersPawn(board,pawnToMove.getCoordinateX(), pawnToMove.getCoordinateY(),activePlayer)){
             System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "That's not your Pawn you dirty cheater! " +
                     "Choose the correct one!" + ConsoleColors.RESET);
             pawnCoordinates = userHandler.choosePawn(board);
             pawnToMove = board[pawnCoordinates[0]][pawnCoordinates[1]];
         }
+
         int pawnPreviousX = pawnToMove.getCoordinateX();
         int pawnPreviousY = pawnToMove.getCoordinateY();
         int[] moveCoordinates = userHandler.getMoveCoordinates(board);
+        while (!validator.isMoveValid(board, pawnToMove.getCoordinateX(), pawnToMove.getCoordinateY(),
+                moveCoordinates[0], moveCoordinates[1])){
+            System.out.println(ConsoleColors.RED_BOLD_BRIGHT + "You cannot move there! " +
+                    "Choose the correct coordinate!" + ConsoleColors.RESET);
+            moveCoordinates = userHandler.getMoveCoordinates(board);
+        }
         pawnToMove.setCoordinateX(moveCoordinates[0]);
         pawnToMove.setCoordinateY(moveCoordinates[1]);
         updateBoard(board, pawnPreviousX, pawnPreviousY, pawnToMove.getCoordinateX(), pawnToMove.getCoordinateY());
         View.clearScreen();
         changeActivePlayer(player1, player2);
-        //TODO continue :)
+        //TODO Pawn hitting
     }
 
     private Pawn[][] initGame(){
